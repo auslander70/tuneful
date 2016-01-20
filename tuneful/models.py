e503a802b1ed4568c106ea5353575820f5403430
Wil DAmato <wil.damato@gmail.com>
2016-01-20 21:33:11 +0000

    add as_dictionary methods to sqlalchemy classes

--- tuneful/models.py
+++ tuneful/models.py
@@ -6,13 +6,23 @@ from sqlalchemy.orm import relationship
 
 from tuneful import app
 from .database import Base, engine
-
+"""
 class Song(Base):
   __tablename__ = "songs"
   
   id = Column(Integer, primary_key=True)
-  
   file = relationship("File", uselist=False, backref="song")
+
+  def as_dictionary(self):
+    song = {
+      "id": self.id,
+      "file": {
+        "id": self.file.id,
+        "name": self.file.filename
+      }
+    }
+    
+  return song
 
 
 class File(Base):
@@ -22,3 +32,5 @@ class File(Base):
   filename = Column(String(512))
   
   song_id = Column(Integer, ForeignKey('songs.id'), nullable=False)
+  
+"""
