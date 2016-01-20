78effa5e10c5576641447181466fa5cfa3cb7e25
Wil DAmato <wil.damato@gmail.com>
2016-01-20 19:35:59 +0000

    adding song and file db models

--- tuneful/models.py
+++ tuneful/models.py
@@ -7,3 +7,18 @@ from sqlalchemy.orm import relationship
 from tuneful import app
 from .database import Base, engine
 
+class Song(Base):
+  __tablename__ = "songs"
+  
+  id = Column(Integer, primary_key=True)
+  
+  file = relationship("File", uselist=False, backref="song")
+
+
+class File(Base):
+  __tablename__ = "files"
+  
+  id = Column(Integer, primary_key=True)
+  filename = Column(String(512))
+  
+  song_id = Column(Integer, ForeignKey('songs.id'), nullable=False)
